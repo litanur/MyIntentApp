993313a83b1b7dd9568cebde0370410c911256bc
Lita Nurlaelati <litanurlaelati@gmail.com>
2022-07-27 18:28:28 +0700

    Latihan Mengirim Data dengan Parcelable

---
+++ app/src/main/java/id/litanur/myintentapp/Person.kt
@@ -0,0 +1,14 @@
+package id.litanur.myintentapp
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Person(
+    val name: String?,
+    val age: Int?,
+    val email: String?,
+    val city: String?
+) : Parcelable
+
+
